c54b467b5b6554a57184ada5fdf6cd661c2978fc
zkovari <zsolt@kovaridev.com>
2022-12-19 12:07:00 +0100

    add item in tests

--- test/test_toolbox.py
+++ test/test_toolbox.py
@@ -1,3 +1,5 @@
+from qtpy.QtWidgets import QLabel
+
 from qttoolbox import ToolBox
 
 
@@ -5,3 +7,5 @@ def test_add_item(qtbot):
     toolbox = ToolBox()
     qtbot.addWidget(toolbox)
     toolbox.show()
+
+    toolbox.addItem(QLabel('Test'), 'Title 1')
