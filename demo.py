818943733e2b4b40c05b3644ca0239bdbbffab04
zkovari <zsolt@kovaridev.com>
2022-12-20 07:01:23 +0100

    fix qtpy import

--- demo.py
+++ demo.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QLabel
+from qtpy.QtWidgets import QApplication, QLabel
 from qtpy.QtWidgets import QMainWindow
 
 from qttoolbox import ToolBox
